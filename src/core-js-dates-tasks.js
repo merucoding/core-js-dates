83cbb0d9378e1fa844d728ff58b4c5bd4d78353c
Meruert <mika.amantaikuzu@gmail.com>
2025-03-20 14:47:53 +0500

    feat: getWeekNumberByDate done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -192,8 +192,10 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const firstDay = new Date(Date.UTC(date.getFullYear()));
+  const diff = Math.ceil((date - firstDay) / 86400000 + 1);
+  return Math.ceil(diff / 7);
 }
 
 /**
