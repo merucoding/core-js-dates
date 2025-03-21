089e1c53fce87159c2c642bebb21e4fb17ac5099
Meruert <mika.amantaikuzu@gmail.com>
2025-03-21 12:05:41 +0500

    feat: isLeapYear done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -296,8 +296,8 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.getTime() - 86400000).getDate() === 29;
 }
 
 module.exports = {
